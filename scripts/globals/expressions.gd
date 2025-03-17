8afbbc7d93c9a08bc813bea8545014ceeba76a58
Jaret-McManus <143516925+Jaret-McManus@users.noreply.github.com>
2025-03-17 11:41:52 -0400

    pulled?

--- scripts/globals/expressions.gd
+++ scripts/globals/expressions.gd
@@ -1,5 +1,13 @@
 extends Node
 
-enum EXPRESSIONS {DEFAULT, EXCITED, SLEEPING, FOCUSED, HAPPY, SKEPTICAL, EMBARRASSED}
+enum EXPRESSIONS {
+	DEFAULT, # OwO
+	EXCITED,  # 'o', >w<
+	SLEEPING, # u.u, u-u, v-v
+	FOCUSED, # omo
+	HAPPY, # :3, :), :]
+	SKEPTICAL, # 0.o
+	EMBARRASSED, 
+	}
 
 var current_expression := EXPRESSIONS.DEFAULT
