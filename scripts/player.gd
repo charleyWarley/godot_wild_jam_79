a4dbc9021f6a5d29e3d6db9427b962d7986d50fa
charleyWarley <ch.jacobs95@gmail.com>
2025-03-18 08:54:51 -0700

    player states updated

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,7 +1,5 @@
 extends CharacterBody2D
 
-#const ACCELERATION := 35
-#const DECELERATION := 850
 
 var current_direction := "down"
 var expression_manager: Node = GlobalNodes.expressions_manager
@@ -14,21 +12,9 @@ func _process(delta: float) -> void:
 	var input : InputPackage = INPUT_MANAGER.gather_input()
 	PLAYER_STATE_MANAGER.update(input, delta)
 	input.queue_free()
-	
-	if Input.is_action_just_pressed("blink"):
-		expression_manager.start_blink()
 
 
 func _physics_process(delta: float) -> void:
 	var input : InputPackage = INPUT_MANAGER.gather_input()
 	PLAYER_STATE_MANAGER.physics_update(input, delta)
 	input.queue_free()
-#
-	#var direction := Input.get_vector("move_left", "move_right", "move_up", "move_down")
-	#set_direction_name(direction)
-	
-#
-
-#func set_direction_name(direction: Vector2) -> void:
-
-#
